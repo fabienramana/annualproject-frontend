3cc00d66bbfd65b84d480639679297f5e5ccfb52
fabien <fabien.rmnd@gmail.com>
2019-12-11 19:01:57 +0100

    add-create-compagny

--- src/app/compagny/compagny-create/compagny-create.component.ts
+++ src/app/compagny/compagny-create/compagny-create.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
 
 @Component({
   selector: 'app-compagny-create',
@@ -6,7 +12,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./compagny-create.component.scss'],
 })
 export class CompagnyCreateComponent {
-  createCompagny(email, pwd) {
-    alert(email.value + " " + pwd.value);
+  constructor(private http: HttpClient) { }
+
+  createCompagny(email, pwd, address, country, siren, activity) {
+    alert(email.value + ' ' + pwd.value + ' ' + address.value + ' ' + country.value + ' ' + siren.value + ' ' + activity.value);
+    const mail = email.value;
+    const password = pwd.value;
+    const addressValue = address.value;
+    const countryValue = country.value;
+    const sirenValue = siren.value;
+    const activityValue = activity.value;
+    const compagny = {
+      mail,
+      password,
+      addressValue,
+      countryValue,
+      sirenValue,
+      activityValue
+    };
+    console.log(compagny);
+    this.http.post('http://localhost:3000/api/create_compagny', compagny)
+        .subscribe(res => console.log(res));
   }
 }
