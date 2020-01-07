a2877996f70d048d4caabac2e64a6046232c11a8
fabien <fabien.rmnd@gmail.com>
2020-01-07 15:33:21 +0100

    add login for both company/admin, and homepage component

--- src/app/compagny/compagny-login/compagny-login.component.ts
+++ src/app/compagny/compagny-login/compagny-login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-compagny-login',
@@ -6,5 +8,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./compagny-login.component.scss'],
 })
 export class CompagnyLoginComponent {
+  constructor(private http: HttpClient, private router: Router) { }
 
+  login(email, pwd) {
+    alert(email.value + ' ' + pwd.value);
+    const mail = email.value;
+    const password = pwd.value;
+    const user = {
+      mail,
+      password
+    };
+    console.log(user);
+    this.http.post<any>('http://localhost:3000/api/login_company', user)
+        .subscribe(res => {
+          // Mettre sécurité si erreur
+            console.log(res);
+            console.log(res.error);
+            if (res.error === 'Error authenticating') {
+              this.http.post<any>('http://localhost:3000/api/login_admin', user)
+                .subscribe(res2 => {
+                  console.log(res2);
+                  if (res2.error === 'Error authenticating') {
+                    console.log('Afficher un message d\'erreur');
+                  }
+                  if (res2.token !== undefined) {
+                    localStorage.setItem('isAdmin', res2.isAdmin);
+                    localStorage.setItem ('token', res2.token);
+                    console.log(localStorage.getItem('token'));
+                    this.router.navigateByUrl('/homepage');
+                  }
+                }
+              );
+            }
+            if (res.token !== undefined) {
+              localStorage.setItem('isAdmin', res.isAdmin);
+              localStorage.setItem ('token', res.token);
+              console.log(localStorage.getItem('token'));
+              this.router.navigateByUrl('/homepage');
+            }
+          }
+        );
+  }
 }
