7d300cf547ae7e83a1b1a2d55df8a654ee69d353
fabien <rmnd.fabien@gmail.com>
2020-01-15 14:07:35 +0100

    create admin component

---
+++ src/app/admin/create-admin/create-admin.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-admin',
+  templateUrl: './create-admin.component.html',
+  styleUrls: ['./create-admin.component.scss']
+})
+export class CreateAdminComponent implements OnInit {
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    this.http.post<any>('http://localhost:3000/api/create_admin', form.value)
+      .subscribe(res => {
+        console.log(res);
+        this.router.navigateByUrl('/homepage');
+      });
+  }
+
+}
